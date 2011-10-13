b3cdd43278a0da483f358a8fd42bbca5ef75615c
Alexandr Zaozerskiy <alexandr.zaozerskiy@lokad.com>
2011-10-13 12:40:15 +0600

    Fixed assembly version issue. Added AssemblyInfo.cs file.

---
+++ dot-net-rest/Source/Lokad.Forecasting.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lokad.Forecasting.Client")]
+[assembly: AssemblyDescription(".NET client to access the Forecasting API v3 (REST)")]
+[assembly: AssemblyCompany("Lokad SAS")]
+[assembly: AssemblyProduct("Lokad.Forecasting.Client")]
+[assembly: AssemblyCopyright("Copyright ©Lokad SAS 2009-2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b4db387d-58de-4b7c-b6fe-a1f23b181a79")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
